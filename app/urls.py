555668be26fb46c9d4c7a772ecbf1af9a8b89bb7
Axel Pion <pionaxel@gmail.com>
2024-04-01 22:28:33 +0200

    refactor: update url to display the base url

--- app/urls.py
+++ app/urls.py
@@ -22,6 +22,6 @@ favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
     path("", include("apk.urls")),
-    path('admin/', admin.site.urls),
+    path('jizo/apk-releaser-admin/', admin.site.urls),
     re_path(r'^favicon\.ico$', favicon_view),
 ]
