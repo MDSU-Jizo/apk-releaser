0aebe3e7dd5afb1e676e447d15a6b0f72171dc23
Axel Pion <pionaxel@gmail.com>
2024-04-01 22:11:49 +0200

    refactor: update url to display the base url

--- app/settings.py
+++ app/settings.py
@@ -151,3 +151,4 @@ WHITENOISE_ALLOW_ALL_ORIGINS = True
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+APPEND_SLASH = True
