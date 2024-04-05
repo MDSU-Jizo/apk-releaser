29745f8525615255a6a101316cec89cb893a37ef
Debian <debian@vps-5cfefde4.vps.ovh.net>
2024-04-05 13:44:17 +0000

    fix: allow to get media file when debug is false

--- app/urls.py
+++ app/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.views.generic.base import RedirectView
+from django.views.static import serve
 from apk.views import index
 from app import settings
 
@@ -27,5 +28,6 @@ urlpatterns = [
     path("", index, name="landing"),
     path('jizo/apk-releaser-admin/', admin.site.urls),
     re_path(r'^favicon\.ico$', favicon_view),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
